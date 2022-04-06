e34131a485f8a1d7662c1d1cf4425ea9d1f61e9b
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-06 12:16:48 -0500

    updated src/HelloWorld.java completed the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,51 @@ public class HelloWorld {
 
     public static void main (String[] args) {
 
-        System.out.println("Hello, World!");
+//        int myFavoriteNumber = 22;
+//
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "3.14159";
+//
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14f;
+//
+//        System.out.println(myNumber);
+
+        /* Post and Pre increment behave differently. Post will increment AFTER the code is executed. Pre will increment firsthand, then follow through the executed code. */
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        int max = Integer.MAX_VALUE;
+
+        max = 2147483648;
+
+        System.out.println(max);
     }
 
 }
