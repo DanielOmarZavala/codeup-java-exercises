f338c5bb579def81e62bcf6ecb72f57a46b82e76
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-06 16:58:34 -0500

    updated src/ConsoleExercises refactored and eliminated traces of multiple scanners

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,38 +4,43 @@ public class ConsoleExercises {
 
     public static void main (String[] args) {
 
-//        double pi = 3.14159;
-//
-//        System.out.printf("The value of pi is approximately %.2f.%n", pi);
-//
-//        Scanner inOne = new Scanner(System.in);
-//        Scanner inTwo = new Scanner(System.in);
-//        Scanner inThree = new Scanner(System.in);
-//
-//        System.out.println("Please enter 3 words");
-//
-//        String strOne = inOne.next();
-//        String strTwo = inTwo.next();
-//        String strThree = inThree.next();
-//
-//        System.out.printf("%s%n%s%n%s%n", strOne, strTwo, strThree);
+        double pi = 3.14159;
 
-//        Scanner sentenceIn = new Scanner(System.in);
-//
-//        System.out.println("Please enter a sentence");
-//
-//        String userSentence = sentenceIn.nextLine();
-//
-//        System.out.printf("You entered: %s%n", userSentence);
+        System.out.printf("The value of pi is approximately %.2f.%n", pi);
 
-        Scanner lengthIn = new Scanner(System.in);
-        Scanner widthIn = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter an integer");
+
+        int numIn = scan.nextInt();
+
+        System.out.printf("%d%n", numIn);
+
+        Scanner inOne = new Scanner(System.in);
+        Scanner inTwo = new Scanner(System.in);
+        Scanner inThree = new Scanner(System.in);
+
+        System.out.println("Please enter 3 words");
+
+        String strOne = scan.next();
+        String strTwo = scan.next();
+        String strThree = scan.next();
+
+        System.out.printf("%s%n%s%n%s%n", strOne, strTwo, strThree);
+
+        Scanner sentenceIn = new Scanner(System.in);
+
+        System.out.println("Please enter a sentence");
+
+        String userSentence = sentenceIn.nextLine();
+
+        System.out.printf("You entered: %s%n", userSentence);
 
 
         System.out.println("Please enter a length and width value");
 
-        int length = Integer.parseInt(lengthIn.next());
-        int width = Integer.parseInt(widthIn.next());
+        int length = Integer.parseInt(scan.next());
+        int width = Integer.parseInt(scan.next());
 
         int area = length * width;
         int perimeter = (length * 2) + (width * 2);
