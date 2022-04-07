1ae99e087698fe4e89896439fae6943b88736868
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-07 15:02:24 -0500

    completed all 4 exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,42 +5,84 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
         Scanner userIn = new Scanner(System.in);
+//
+//        String userChoice = "";
+//
+//
+//        do {
+//
+//            System.out.println("Would you like to try this program? Y/N");
+//
+//            userChoice = userIn.next();
+//
+//            if (userChoice.equalsIgnoreCase("y")) {
+//                System.out.print("What number would you like to go up to? ");
+//
+//                int numIn = userIn.nextInt();
+//
+//                System.out.println("");
+//
+//                System.out.println("Here is your table:");
+//
+//                System.out.println("");
+//
+//                System.out.println("number | squared | cubed");
+//                System.out.println("------ | ------- | -----");
+//
+//                for (int i = 1; i <= numIn; i++) {
+//
+//                    int squared = i * i;
+//                    int cubed = (i * i) * i;
+//
+//                    if (squared >= 10 && cubed >= 10) {
+//                        System.out.printf("%d      | %d      | %d      %n", i, squared, cubed);
+//                        continue;
+//                    }
+//
+//                    System.out.printf("%d      | %d       | %d       %n", i, squared, cubed);
+//
+//
+//                }
+//
+//            } else {
+//                System.out.println("Thank you for your response, resetting welcome message...");
+//            }
+//
+//        } while (userChoice.equalsIgnoreCase("n"));
+
 
         String userChoice = "";
 
-
         do {
 
-            System.out.println("Would you like to try this program? Y/N");
+            System.out.print("Would you like to try this program? Y/N: ");
 
             userChoice = userIn.next();
 
             if (userChoice.equalsIgnoreCase("y")) {
-                System.out.print("What number would you like to go up to? ");
 
-                int numIn = userIn.nextInt();
+                System.out.print("Please enter a numerical grade: ");
+                int numGrade = userIn.nextInt();
 
-                System.out.println("");
+                if (numGrade >= 88) {
 
-                System.out.println("Here is your table:");
+                    System.out.println("You got an A! Congratulations!");
 
-                System.out.println("");
+                } else if (numGrade >= 80) {
 
-                System.out.println("number | squared | cubed");
-                System.out.println("------ | ------- | -----");
+                    System.out.println("You got an B! Congratulations!");
 
-                for (int i = 1; i <= numIn; i++) {
+                } else if (numGrade >= 67) {
 
-                    int squared = i * i;
-                    int cubed = (i * i) * i;
+                    System.out.println("You got an C! Close Call!");
 
-                    if (squared >= 10 && cubed >= 10) {
-                        System.out.printf("%d      | %d      | %d      %n", i, squared, cubed);
-                        continue;
-                    }
+                } else if (numGrade >= 60) {
 
-                    System.out.printf("%d      | %d       | %d       %n", i, squared, cubed);
+                    System.out.println("You got an D! Better Luck Next Time!");
 
+                } else if (numGrade >= 0) {
+
+                    System.out.println("You got an F! SHAME!");
 
                 }
 
@@ -49,6 +91,7 @@ public class ControlFlowExercises {
             }
 
         } while (userChoice.equalsIgnoreCase("n"));
+
 
     }
 }
