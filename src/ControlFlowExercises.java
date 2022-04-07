dd1973b5b772b036230bd4d4cf0e1d7324a68fa6
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-07 14:05:48 -0500

    completed second exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,45 +2,21 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-//        int i = 5;
-//
-//        while (i <= 15) {
-//
-//            System.out.print(i + " ");
-//
-//            ++i;
-//        }
+        for (int i = 1; i <= 100; i++) {
 
 
-        for (int i = 5; i <= 15; i++) {
-            System.out.print(i + " ");
-        }
+            if (i % 5 == 0 & i % 3 == 0) {
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            }
 
-        System.out.println("");
-        System.out.println("");
-
-
-//        int counter = 100;
-//
-//        do {
-//            System.out.println(counter);
-//            counter -= 5;
-//        } while (counter >= -10);
-
-        for (int i = 100; i >= -10; i -= 5) {
             System.out.println(i);
         }
-
-//        long secondCounter = 2L;
-//
-//        do {
-//            System.out.println(secondCounter);
-//            secondCounter *= secondCounter;
-//        } while (secondCounter < 1000000);
-
-        for (long i = 2L; i < 1000000; i *= i) {
-            System.out.println(i);
-        }
-
     }
 }
