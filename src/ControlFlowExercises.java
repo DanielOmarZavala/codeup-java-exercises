27c5c7a52abb09fc5ba0c7bc54f11643f6b42640
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-07 14:41:09 -0500

    completed exercise 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,22 +1,54 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        Scanner userIn = new Scanner(System.in);
+
+        String userChoice = "";
 
 
-            if (i % 5 == 0 & i % 3 == 0) {
-                System.out.println("FizzBuzz");
-                continue;
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-                continue;
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-                continue;
+        do {
+
+            System.out.println("Would you like to try this program? Y/N");
+
+            userChoice = userIn.next();
+
+            if (userChoice.equalsIgnoreCase("y")) {
+                System.out.print("What number would you like to go up to? ");
+
+                int numIn = userIn.nextInt();
+
+                System.out.println("");
+
+                System.out.println("Here is your table:");
+
+                System.out.println("");
+
+                System.out.println("number | squared | cubed");
+                System.out.println("------ | ------- | -----");
+
+                for (int i = 1; i <= numIn; i++) {
+
+                    int squared = i * i;
+                    int cubed = (i * i) * i;
+
+                    if (squared >= 10 && cubed >= 10) {
+                        System.out.printf("%d      | %d      | %d      %n", i, squared, cubed);
+                        continue;
+                    }
+
+                    System.out.printf("%d      | %d       | %d       %n", i, squared, cubed);
+
+
+                }
+
+            } else {
+                System.out.println("Thank you for your response, resetting welcome message...");
             }
 
-            System.out.println(i);
-        }
+        } while (userChoice.equalsIgnoreCase("n"));
+
     }
 }
