ae1d46a39c657c2a3198c09ff2242e9e26aaed81
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-08 18:05:34 -0500

    updated src/MethodsExercises.java completed exercise 3/5

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class MethodsExercises {
 
 
-
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        /* First Exercise */
 //        System.out.println(addition(2, 3));
 //
 //        System.out.println(subtraction(2, 3));
@@ -16,16 +18,38 @@ public class MethodsExercises {
 //
 //        System.out.println(modulus(14, 3));
 
-        int userInput = getInteger(1, 10);
+        /* Second Exercise */
+//        getInteger(1, 10);
 
-        if (userInput >= 1 & userInput <= 10) {
-            System.out.println("Your number is between 1 and 10...WOW!");
-        } else {
-            System.out.println("Sorry, your number is NOT between 1 and 10!");
-        }
+
+        /* Third Exercise */
+        String factorialContinue = "";
+
+        do {
+
+            System.out.print("Would you like to try the factorial program? Y/N: ");
+
+            factorialContinue = scan.next();
+
+            if (factorialContinue.equalsIgnoreCase("y")) {
+
+                System.out.print("Enter a number between 1 and 10 again please: ");
+
+                long userNum = scan.nextInt();
+
+                checkInteger(userNum);
+
+            } else {
+
+                System.out.println("Goodbye!");
+
+            }
+
+        } while (factorialContinue.equalsIgnoreCase("y"));
+
 
     }
-
+    /* First Exercise */
 //    public static int addition(int numOne, int numTwo) {
 //        return numOne + numTwo;
 //    }
@@ -46,15 +70,54 @@ public class MethodsExercises {
 //        return numOne % numTwo;
 //    }
 
-    public static int getInteger(int min, int max) {
+    /* Second Exercise */
+//    public static void getInteger(int min, int max) {
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter a number between 1 and 10: ");
+//
+//        int userNum = scan.nextInt();
+//
+//        if (userNum >= 1 & userNum <= 10) {
+//
+//            System.out.println("Your number is between 1 and 10...WOW!");
+//
+//        } else {
+//
+//            System.out.println("Sorry, your number is NOT between 1 and 10!");
+//
+//        }
+//
+//    }
 
-        Scanner scan = new Scanner(System.in);
+    /* Third Exercise */
+    public static void checkInteger(long userNum) {
 
-        System.out.print("Enter a number between 1 and 10: ");
+        if (userNum >= 1 & userNum <= 10) {
 
-        int userNum = scan.nextInt();
+            System.out.println("Awesome, your number is between 1 and 10, perfect!");
 
-        return userNum;
+            System.out.println("Your number's factorial is: " + factorialConverter(userNum));
+
+        } else {
+
+            System.out.println("Sorry, your number is NOT between 1 and 10!\nTry again!");
+        }
 
     }
+
+    public static long factorialConverter(long userNum) {
+
+        long factorial = 1;
+
+        for (int i = 2; i <= userNum; i++) {
+
+            factorial = factorial * i;
+
+        }
+
+        return factorial;
+    }
+
 }
