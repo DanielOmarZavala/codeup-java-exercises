310be50497b14619d1cf8024a1f81903d37eda47
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-11 16:19:19 -0500

    added src/Person.java init commit

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+    }
+}
+
+/* The class should have a constructor that accepts a `String` value and sets
+the person's name to the passed string.
+
+Create a `main` method on the class that creates a new `Person` object and
+tests the above methods. */
